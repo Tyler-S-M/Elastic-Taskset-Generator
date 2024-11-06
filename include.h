27a65ffaf5d83ffdcda8ece87796e6868d806146
Tyler <martin.t@wustl.edu>
2024-11-06 16:21:41 -0600

    removed not needed files

--- include.h
+++
@@ -1,7 +0,0 @@
-#ifndef _include_h
-#define _include_h
-const int NUMCPUS = 15;//There is always 1 CPU set aside for the scheduler object.
-const int maxSMS = 15;
-const int MAXTASKS = 10;
-const int MAXMODES = 10;
-#endif
